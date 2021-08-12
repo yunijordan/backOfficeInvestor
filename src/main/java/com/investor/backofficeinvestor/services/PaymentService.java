659f8c8287dd195580972542f16d5b48d2ce91e8
yunijordan <Alex2013*/>
2021-08-11 20:01:53 -0400

    hit to amazon ultimate 11/8

--- src/main/java/com/investor/backofficeinvestor/services/PaymentService.java
+++ src/main/java/com/investor/backofficeinvestor/services/PaymentService.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-
 @Service
 @Transactional
 public class PaymentService {
